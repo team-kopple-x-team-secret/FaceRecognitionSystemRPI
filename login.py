bee49fafd7b3ebb9cc688893c633d5cb9dda7b5e
EquinoxGneiss <cruz34621@gmail.com>
2023-04-08 01:30:39 +0800

    test

--- login.py
+++ login.py
@@ -20,24 +20,13 @@ class Login:
         font_style = ("Rubik", 16)
 
         self.username_entry = tk.Entry(self.root, width=30, bg="#D8DEE9", bd=0, highlightthickness=0, font=font_style)
-        self.username_entry.place(x=571, y=288, width=316, height=60)
+        self.username_entry.place(x=828, y=443, width=370, height=70)
 
         self.password_entry = tk.Entry(self.root, show="*", width=30, bg="#D8DEE9", bd=0, highlightthickness=0, font=font_style)
-        self.password_entry.place(x=571, y=384, width=316, height=60)
+        self.password_entry.place(x=829, y=566, width=370, height=70)
 
         login_button = tk.Button(self.root, text="Login", bg="#3E806E", fg="#ECEFF4", bd=0, highlightthickness=0, font=font_style, command=self.login)
-        login_button.place(x=479, y=509, width=408, height=60)
-
-        canvas.create_rectangle(479, 288, 571, 348, fill="#BF616A", outline="")
-        canvas.create_rectangle(479, 384, 571, 444, fill="#BF616A", outline="")
-
-        username_icon = Image.open("src/login/icon_username.png")
-        username_photo = ImageTk.PhotoImage(username_icon)
-        canvas.create_image(511, 302, image=username_photo, anchor="nw")
-
-        password_icon = Image.open("src/login/icon_password.png")
-        password_photo = ImageTk.PhotoImage(password_icon)
-        canvas.create_image(511, 398, image=password_photo, anchor="nw")
+        login_button.place(x=721, y=718, width=475, height=70)
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
